dc93289ad8dfc4d140cdfc0888f96d1e105bccb4
manuniia <mashullyyy@gmail.com>
2024-10-14 11:36:00 -0400

    add sessions

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const app = express();
+const session = require("express-session");
+const SQLiteStore = require("connect-sqlite3")(session);
 
-var indexRouter = require("./routes/index");
-
-var app = express();
+const indexRouter = require("./routes/index");
+const {
+  DB_FILE_NAME,
+  DB_FILE_FOLDER,
+  SESSION_SECRET,
+  SESSION_COOKIE_MAX_AGE,
+} = require("./const");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,6 +23,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+
+app.use(
+  session({
+    store: new SQLiteStore({
+      db: DB_FILE_NAME,
+      dir: DB_FILE_FOLDER,
+    }),
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: SESSION_COOKIE_MAX_AGE,
+  })
+);
 
 app.use("/", indexRouter);
 
--- const/index.js
+++ const/index.js
@@ -1,4 +1,18 @@
-const SALT_ROUNDS = 10;
-const DB_FILE_PATH = "./db/db.sqlite";
+require("dotenv").config();
 
-module.exports = { SALT_ROUNDS, DB_FILE_PATH };
+const SALT_ROUNDS = 10;
+const DB_FILE_NAME = "db.sqlite";
+const DB_FILE_FOLDER = "./db";
+const SESSION_COOKIE_MAX_AGE = 7 * 24 * 60 * 60 * 1000; // 1 week
+
+const DB_FILE_PATH = `${DB_FILE_FOLDER}${DB_FILE_NAME}`;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+
+module.exports = {
+  SALT_ROUNDS,
+  DB_FILE_PATH,
+  DB_FILE_NAME,
+  DB_FILE_FOLDER,
+  SESSION_SECRET,
+  SESSION_COOKIE_MAX_AGE,
+};
