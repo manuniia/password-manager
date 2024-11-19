c8fa628cadc633b3a3216cd4fcbc73bfa48fb256
manuniia <mashullyyy@gmail.com>
2024-11-18 21:00:51 -0500

    remove listen call

--- app.js
+++ app.js
@@ -57,9 +57,4 @@ app.engine("jsx", require("express-react-views").createEngine());
 
 app.use("/", indexRouter);
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
 module.exports = app;
