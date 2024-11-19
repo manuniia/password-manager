db50e9e62e69f6e42e18c4436074eba5d81814f2
manuniia <mashullyyy@gmail.com>
2024-11-18 21:10:07 -0500

    add health check endpoint

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,11 @@ router.get("/", async function (req, res, _next) {
 router.use("/", authRouter);
 router.use("/", isAuth, passwordsRouter);
 
+router.get("/healthz", async function (req, res, _next) {
+  console.warn("Health check!");
+  return res.status(200);
+});
+
 router.get("*", async function (req, res, _next) {
   res.render("not-found", { user: req.session.user });
 });
